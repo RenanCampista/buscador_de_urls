b45a6a8d50a553eb7a0f87e1ef1cc3646f85a48f
RenanCampista <renan.campista@edu.ufes.br>
2024-09-26 16:57:46 -0300

    refactor: utiliza o bing como motor de busca principal

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-MIN_TEXT_LENGTH = 200
-MAX_TEXT_LENGTH = 3500
-
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
@@ -22,34 +19,40 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/91.0.864.48",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 ]
+MIN_TEXT_LENGTH = 200
 
-SEARCH_ENGINES = [
-    f"https://www.google.com/search?q=",
-    f"https://duckduckgo.com/html/?q=",
-    f"https://www.bing.com/search?q=",
-    f"https://search.yahoo.com/search?p=",
-    f"https://www.ask.com/web?q="
-]
+
+class SearchEngine(Enum):
+    """The search engines used to search for the posts."""
+    BING = ("Bing", "https://www.bing.com/search?q=")
+    DUCKDUCKGO = ("DuckDuckGo", "https://duckduckgo.com/html/?q=")
+    GOOGLE = ("Google", "https://www.google.com/search?q=")
+    
+    def get_name(self) -> str:
+        return self.value[0]
+    
+    def get_url(self) -> str:
+        return self.value[1]
 
 
 class SocialNetwork(Enum):
     """The social networks for which the URLs are extracted."""
-    FACEBOOK = ("Caption", "site: facebook.com", "https://www.facebook.com/", "URL", ["posts", "videos", "photos", "groups"])
-    INSTAGRAM = ("Caption", "site: instagram.com", "https://www.instagram.com/", "URL", ["p/", "tv/", "reel/", "video/", "photo/"])
+    FACEBOOK = ("https://www.facebook.com/",  ["posts/", "videos/", "photos/", "groups/"], "Caption", "URL", "site:+facebook.com")
+    INSTAGRAM = ("https://www.instagram.com/", ["p/", "tv/", "reel/", "video/", "photo/"], "Caption", "URL", "site:+instagram.com")
     
-    def get_text_column(self) -> str:
+    def get_domain_url(self) -> str:
         return self.value[0]
     
-    def get_url_query(self) -> str:
+    def get_valid_substrings(self) -> list:
         return self.value[1]
     
-    def get_post_url(self) -> str:
+    def get_text_column(self) -> str:
         return self.value[2]
     
     def get_url_column(self) -> str:
         return self.value[3]
     
-    def get_valid_substrings(self) -> list:
+    def get_site_query(self) -> str:
         return self.value[4]
     
 
@@ -112,31 +115,28 @@ def filter_bmp_characters(text: str) -> str:
     return re.sub(r'[^\u0000-\uFFFF]', '', text)
 
 
-def search_with_requests(query: str, social_network: SocialNetwork) -> str:
-    """Performs a search using requests and BeautifulSoup and returns the first matching URL."""
+def search_with_requests(query: str, social_network: SocialNetwork) -> tuple:
+    """Performs a search using requests and BeautifulSoup and returns the first matching URL and the search engine used."""
     headers = {
         "User-Agent": random.choice(USER_AGENTS),
     }
     
-    query_filtered = filter_bmp_characters(query)
-    search_engines = [engine + query_filtered for engine in SEARCH_ENGINES]
-    random.shuffle(search_engines) # Randomize the search order
+    query_filtered = filter_bmp_characters(query).replace(' ', '+')
+    search_engines = [(engine.get_url() + query_filtered, engine.get_name())
+                      for engine in SearchEngine]
     
-    for search_url in search_engines:
-        response = requests.get(
-            url=search_url, 
-            headers=headers
-        )
+    for search_url, engine_name in search_engines:
+        response = requests.get(search_url, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
         
         for link in soup.find_all('a', href=True):
             href = link['href']
-            
-            if social_network.get_post_url() in href and \
+            if social_network.get_domain_url() in href and \
                 any(substring in href for substring in social_network.get_valid_substrings()):
-                return href
+                return href, engine_name
+            
         time.sleep(random.uniform(1, 3)) 
-    return ''
+    return '', ''
 
 
 def extract_relevant_url(url: str) -> str:
@@ -155,30 +155,34 @@ def main():
     data_posts[social_network.get_url_column()] = ''
     
     search_success = 0
+    insuficient_text = 0
     for index, row in data_posts.iterrows():
         text = row[social_network.get_text_column()]
-        query = f"{social_network.get_url_query()}+{text}"
+        query = f"{social_network.get_site_query()}+{text}"
         
-        if not MIN_TEXT_LENGTH <= len(query) <= MAX_TEXT_LENGTH:
+        if not MIN_TEXT_LENGTH <= len(query):
             print(f"Texto da linha {index + 1} ignorado (comprimento inválido: {len(query)} caracteres)")
+            insuficient_text += 1
             continue
         
-        url = search_with_requests(query, social_network)
-        relevant_url = extract_relevant_url(url) if url else ''
-        data_posts.at[index, social_network.get_url_column()] = relevant_url
-        
+        url, se = search_with_requests(query, social_network)
+        relevant_url = ''
+    
         if url:
-            print(f"Post da linha {index + 1} encontrado: {relevant_url}")
+            relevant_url = extract_relevant_url(url)
+            print(f"Post da linha {index + 1} encontrado no {se}: {relevant_url}")
             search_success += 1
         else:
             print(f"Post da linha {index + 1} não encontrado.")
-        
+            
+        data_posts.at[index, social_network.get_url_column()] = relevant_url
         time.sleep(random.uniform(2, 5))
     
-    print(f"Busca finalizada. URLs encontradas: {search_success}/{len(data_posts)}")
+    print(f"\nBusca finalizada. URLs encontradas: {search_success}/{len(data_posts)}")
+    print(f"Posts ignorados por texto insuficiente (menos de {MIN_TEXT_LENGTH} caracteres): {insuficient_text}")
+    print(f"Posts pesquisados sem sucesso: {len(data_posts) - search_success - insuficient_text}")
     data_posts.to_csv(f'{file_name[:-4]}_with_urls.csv', index=False)
 
 
 if __name__ == '__main__':
     main()
-
